660178882646fe55296bb607906b8618191f6606
SamuelBaronAbad <peterpunkbaron@gmail.com>
2021-01-04 20:11:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { DatePicker } from 'antd';
+import './App.scss';
 
 function App() {
+
+  const test = (date, dateString) => {
+    console.log(date, dateString);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Web personal - Client <span>Samuel</span></h1>
+      <DatePicker onChange={test}/>
+      <h2>Proyecto</h2>
     </div>
+
   );
 }
 
