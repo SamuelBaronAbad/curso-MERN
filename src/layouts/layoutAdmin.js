653519f46ba40833d84bc382a8d142d5f7496f09
SamuelBaronAbad <peterpunkbaron@gmail.com>
2021-01-11 14:27:21 +0100

    Menu Sider and Top

--- src/layouts/layoutAdmin.js
+++ src/layouts/layoutAdmin.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from "antd";
+import MenuTop from '../components/Admin/MenuTop';
+import MenuSider from '../components/Admin/MenuSider';
 
 import "./layoutAdmin.scss";
 
 export default function LayoutAdmin(props) {
     const { routes } = props;
+    const [menuCollapsed, setMenuCollapsed] = useState(false);
     const { Header, Content, Footer } = Layout;
 
     return (
         <Layout>
-            <h2>Menu Sider Admin</h2>
-            <Layout>
-                <Header>header...</Header>
-                <Content>
+           <MenuSider menuCollapsed={menuCollapsed}></MenuSider>
+            <Layout className="layout-admin" style={{marginLeft: menuCollapsed ? "80px" : "200px"}}>
+                <Header className="layout-admin__header">
+                      <MenuTop menuCollapsed={menuCollapsed} setMenuCollapsed={setMenuCollapsed}></MenuTop>
+                </Header>
+                <Content className="layout-admin__content">
                     <LoadRoutes routes={routes} />
                 </Content>
-                <Footer>
+                <Footer className="layout-admin__footer">
                     Samuel Baron Abad 2021
                 </Footer>
             </Layout>
-
         </Layout>
     )
 }
